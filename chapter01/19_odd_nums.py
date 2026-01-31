3232fa6d1deaee23b8545eb818830331580ce2b1
serendipity019 <navagio09@gmail.com>
2026-01-31 20:19:07 +0200

    We saw an example with for loop, java style and compared to Python style

---
+++ chapter01/19_odd_nums.py
@@ -0,0 +1,11 @@
+print("Java Style")
+for i in range(22):
+    if i % 2 != 0:
+        print(i, end=" ")
+print()
+
+print("Python Style")
+for i in range(1, 22, 2):
+    print(i, end=" ")
+print()
+
