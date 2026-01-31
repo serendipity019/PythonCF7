8ccf41b82c02ed8e6b576519f43aabccdb15229b
serendipity019 <navagio09@gmail.com>
2026-01-31 18:07:34 +0200

    We saw example with floats in the form of floate-x

---
+++ 10_floats_demo.py
@@ -0,0 +1,10 @@
+PI = 3.1415926535
+
+print("Value of PI:", PI)
+print("Type of PI:", type(PI))
+
+small_float = 2.5e-3 # 0.0025 or 2.5 x 10^-3
+
+print("Small float value:", small_float)
+print("Type of small_float:", type(small_float))
+
