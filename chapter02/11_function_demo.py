5d3a78065ee576d5804bb3659dc9df08a28fdc49
serendipity019 <navagio09@gmail.com>
2026-02-06 19:10:20 +0200

    We saw an example how to make functions that have documantation and urge for the type that use

---
+++ chapter02/11_function_demo.py
@@ -0,0 +1,15 @@
+def say_hello(name: str = "World"):
+    """
+    Print a greeting message.
+    
+    Args:
+        name (str): The name to greet. Default value is "World".
+    """
+    print(f"Hello, {name}!")
+
+def main():
+    say_hello("Coding Factory")
+
+if __name__ == "__main__":
+    main()
+
