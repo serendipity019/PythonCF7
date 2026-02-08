75c3b17a3082a0b04f1dd9976bf533dc59598394
serendipity019 <navagio09@gmail.com>
2026-02-08 21:53:57 +0200

    We saw how to use getters and setters and how to declare classmethod

--- chapter02/22_point_demo.py
+++ chapter02/22_point_demo.py
@@ -1,36 +1,61 @@
 class Point:
     __count = 0 # We use duble underscore for "private"
 
-def __init__(self, x = 0, y = 0):
-    self.__x = x
-    self.__y = y
-    Point.__count += 1
+    def __init__(self, x = 0, y = 0):
+        self.__x = x
+        self.__y = y
+        Point.__count += 1
 
-def __str__(self):
-    return f"({self.__x}, {self.__y})"    
+    def __str__(self):
+        return f"({self.__x}, {self.__y})"    
+        
+    def __repr__(self): # This is for debugging representation
+        return f"Point(x={self.__x}, y={self.__y})"
+
+    def __eq__(self, other):
+        if isinstance(other, Point):
+            return self.__x == other.__x and self.__y == other.__y
+        else:
+            return False
     
-def __repr__(self): # This is for debugging representation
-    return f"Point(x={self.__x}, y={self.__y})"
+    # def __lt__, __gt__
+    @classmethod
+    def get_count(cls):
+        return cls.__count    
 
-def __eq__(self, other):
-    if isinstance(other, Point):
-        return self.__x == other.__x and self.__y == other.__y
-    else:
-        return False
-    
-# def __lt__, __gt__
-@classmethod
-def get_count(cls):
-    return cls.__count    
+    @property
+    def x(self): # This work as getter in Java
+        return self.__x
 
-@property
-def x(self): # This work as getter in Java
-    return self.__x
+    @x.setter
+    def x(self, x): # This work as setter in Java
+        self.__x = x
+        
+    @property
+    def y(self):
+        return self.__y
 
-@x.setter
-def x(self, x): # This work as setter in Java
-    self.__x = x
-    
+    @y.setter
+    def y(self, y):
+        self.__y = y
+
+def main():
+    p1 = Point(10, 20)
+    p2 = Point(11, 20)
+
+    print(p1)
+    print(p2)
+
+    print(f"p1 == p2: {p1 == p2}")
+
+    p2.x = 10
+    print(p1.x, p1.y)
+    print(f"p1 == p2: {p1 == p2}")
+
+    print(Point.get_count)
+
+if __name__ == "__main__":
+    main()
     
 
 
