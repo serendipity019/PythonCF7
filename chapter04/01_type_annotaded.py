cc23c3f726379324d8aa941ac77beab6b48f7cd5
serendipity019 <navagio09@gmail.com>
2026-02-11 18:42:25 +0200

    We saw function with 2 types of every variable and how we can see the doc and the annotation of any function

---
+++ chapter04/01_type_annotaded.py
@@ -0,0 +1,36 @@
+def my_add(a: float | int, b: float | int) -> float | int:
+    """
+    Adds two numbers and returns the result.
+
+    Args:
+        a (int, float): The first number to add.
+        b (int, float): The second number to add.
+    
+    Returns:
+        int |float: The sum of 'a' and 'b'
+
+    Raise:
+        TypeError: If either 'a' or 'b' is not an integer or float.
+    """
+
+    if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+        raise TypeError("Both a and b must be integers or floats")
+    
+    return a + b
+
+def main():
+    print(my_add(10, 20.5))
+    
+    try:
+        print(my_add(10, "hello"))
+    except TypeError as e:
+        print(e)
+        return
+    
+    print("Annotations:", my_add.__annotations__)
+    print("Docs:", my_add.__doc__)
+    print("-" * 20)
+    help(my_add)
+
+if __name__ == "__main__":
+    main()
