0b4836167691e1de2e3577fc574696ceb4c433c7
serendipity019 <navagio09@gmail.com>
2026-02-16 22:38:40 +0200

    We use from library functools the lru_cache to see how the fibonacci recurcive function work like the example before(module 22_...)

---
+++ chapter05/23_lru_func.py
@@ -0,0 +1,22 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def fibo(n: int) -> int:
+   if n in (0,1): return n
+   return fibo(n-1) + fibo(n-2) 
+
+def fibo_with_logging(n):
+   if fibo.cache_info().hits > 0:
+      print(f"Cache hit for Fibo({n})")
+   else:
+      print(f"Calculating Fibo({n})")
+
+def main():
+   fibo.cache_clear()
+
+   fibo_nums = [fibo_with_logging(n) for n in range(21)]
+   print(fibo_nums)
+
+if __name__ == "__main__":
+    main()
+
