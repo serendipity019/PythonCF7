a544768ca5fa603d4e7cbe4512b3720ea3856b3c
serendipity019 <navagio09@gmail.com>
2026-02-16 22:26:46 +0200

    We saw a memoize decorator fot a recurcive fibonacci function

---
+++ chapter05/22_memoize_fibo.py
@@ -0,0 +1,37 @@
+def memoize(func):
+    """  
+    A simple memoization decorator to cache results of the function.
+
+    Param:
+        func (function): The import function 
+    """
+    cache = {}
+    #cache_stats = {"hits": 0, "misses": 0}
+
+    def wrapper(n):
+        if n in cache:
+            #cache_stats["hits"] += 1
+            print(f"Cache hit for Fibo({n})")
+        else:
+            #cache_stats["misses"] += 1
+            print(f"Calculating Fibo({n})")
+            cache[n] = func(n)
+        return cache[n]
+    
+    return wrapper
+
+def fibo(n: int) -> int:
+   if n in (0,1): return n
+   return fibo(n-1) + fibo(n-2) 
+
+@memoize
+def fibo2(n: int) -> int:
+   if n in (0,1): return n
+   return fibo(n-1) + fibo(n-2) 
+
+def main():
+    print([fibo(n) for n in range(20)])
+    print([fibo2(n) for n in range(20)])
+
+if __name__ == "__main__":
+    main() 
