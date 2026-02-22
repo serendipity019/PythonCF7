854bbb20008d6318e89f20ba1d618041fc38b61d
serendipity019 <navagio09@gmail.com>
2026-02-22 19:50:11 +0200

    We saw an example with custom exception

---
+++ chapter05/38_custom_errors/store.py
@@ -0,0 +1,55 @@
+class OutOfStockError(Exception):
+    def __init__(self, item_name):
+        super().__init__(f"{item_name} is out of stock.")
+
+class Item:
+    def __init__(self, name, quantity):
+        self.name = name
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"{self.name}, {self.quantity}"
+    
+    __repr__ = __str__
+
+    def __eq__(self, other):
+        if not isinstance(other, Item):
+            return False
+        return self.name == other.name
+    
+    def __hash__(self):
+        return hash(self.name)
+    
+class Inventory:
+    def __init__(self):
+        self.items = []
+    
+    def add_item(self, item):
+        for existing_item in self.items:
+            if existing_item == item:
+                existing_item.quantity += 1 # item.quantity
+                return
+        self.items.append(item)
+
+    def remove_item(self, item_name_to_remove):
+        if Item(item_name_to_remove, None) not in self.items:
+            raise ValueError(f"{item_name_to_remove} is not in the inventory.")
+        
+        for item in self.items:
+            if item == Item(item_name_to_remove, None):
+                if item.quantity > 0:
+                    item.quantity -= 1
+                    return Item(item.name, item.quantity)
+                else: 
+                    raise OutOfStockError(item.name)
+    
+    def print_items(self):
+        for item in self.items:
+            print(item)
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()  
